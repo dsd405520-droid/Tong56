774d459fb61e8d632141721c7e444b915db32fb3
thanongsaiduangdala <thanongsai.duangdala@gmail.com>
2026-08-05 14:14:29 +0700

    sdasdsa

--- API/server.py
+++ API/server.py
@@ -1,11 +1,28 @@
 from fastapi import FastAPI
+import importlib
+import os
+from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+load_dotenv()
 
-app = FastAPI()
+app = FastAPI(title="FastAPI Back-end System")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello, World!"}
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+ROUTER_DIR = "router"
+if os.path.exists(ROUTER_DIR):
+    for root, dirs, files in os.walk(ROUTER_DIR):
+        for filename in files:
+            if filename.endswith(".py") and filename != "__init__.py":
+                filepath = os.path.join(root, filename)
+                module_name = filepath.replace(os.sep, ".")[:-3]
+                route_module = importlib.import_module(module_name)
+                if hasattr(route_module, "router"):
+                    app.include_router(route_module.router)
+else:
+    print(f"Warning: Directory {ROUTER_DIR} not found")
